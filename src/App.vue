82311981a7cde9f6e6bf7d67d4cd695f47562c21
MANISHKENGUVA <vijayakenguva@gmail.com>
2025-04-28 18:44:30 +0530

    CCCCCCCCCCCCCCCCCCCCC

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,33 @@
 <template>
   <div>
-    <h1>Dummy PDF Generator (Working on Safari, Chrome, Firefox)</h1>
-    <button @click="createDummyPdf(false)">Open Dummy PDF</button>
-    <button @click="createDummyPdf(true)">Download Dummy PDF</button>
+    <h1>Google Drive PDF Open/Download</h1>
+    <button @click="downloadPdfFromDrive(false)">Open PDF</button>
+    <button @click="downloadPdfFromDrive(true)">Download PDF</button>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    createDummyPdf(isDownload) {
-      try {
-        // 1. Very minimal valid PDF structure
-        const pdfContent = `%PDF-1.4
-1 0 obj
-<< /Type /Catalog /Pages 2 0 R >>
-endobj
-2 0 obj
-<< /Type /Pages /Kids [3 0 R] /Count 1 >>
-endobj
-3 0 obj
-<< /Type /Page /Parent 2 0 R /MediaBox [0 0 612 792] /Contents 4 0 R /Resources << >> >>
-endobj
-4 0 obj
-<< /Length 44 >>
-stream
-BT
-/F1 24 Tf
-100 700 Td
-(Dummy PDF) Tj
-ET
-endstream
-endobj
-xref
-0 5
-0000000000 65535 f 
-0000000010 00000 n 
-0000000060 00000 n 
-0000000117 00000 n 
-0000000213 00000 n 
-trailer
-<< /Root 1 0 R /Size 5 >>
-startxref
-312
-%%EOF`;
+    downloadPdfFromDrive(isDownload) {
+      const fileId = "13nrItpG_p_evAkfzUO2zd-FcViXprXx1"; // Your Google Drive file ID
+      const downloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
 
-        // 2. Convert to bytes
-        const byteCharacters = unescape(encodeURIComponent(pdfContent));
-        const byteNumbers = new Array(byteCharacters.length);
-        for (let i = 0; i < byteCharacters.length; i++) {
-          byteNumbers[i] = byteCharacters.charCodeAt(i);
-        }
-        const byteArray = new Uint8Array(byteNumbers);
+      const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-        // 3. Create Blob
-        const pdfBlob = new Blob([byteArray], { type: "application/pdf" });
-
-        // 4. Create Blob URL
-        const blobURL = URL.createObjectURL(pdfBlob);
-
-        // 5. Detect if browser is Safari
-        const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-        if (isDownload) {
-          if (isSafari) {
-            // Safari doesn't support link.download with blob URLs very well
-            // Instead, open it and let user manually save
-            window.open(blobURL, "_blank");
-          } else {
-            const link = document.createElement("a");
-            link.href = blobURL;
-            link.download = "dummy.pdf"; // filename
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-          }
+      if (isDownload) {
+        if (isSafari) {
+          window.open(downloadUrl, "_blank"); // Safari opens, then user saves
         } else {
-          window.open(blobURL, "_blank");
+          const link = document.createElement('a');
+          link.href = downloadUrl;
+          link.download = "document.pdf"; // Download filename
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
         }
-      } catch (error) {
-        console.error("Error creating dummy PDF:", error);
+      } else {
+        window.open(downloadUrl, "_blank"); // Open in new tab
       }
     }
   }
